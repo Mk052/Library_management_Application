f8967c8d87b43d6fc5771d38055bd50db85d0e97
madhu <madhukush006@gmail.com>
2025-02-19 12:17:47 +0530

    Update the Student serializer class

--- Bookmanagement/serializers.py
+++ Bookmanagement/serializers.py
@@ -5,10 +5,22 @@ from Bookmanagement.models import Author, Category, Course, Student
 
 class StudentSerializers(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    course = serializers.StringRelatedField(read_only=True)
+    course_id = serializers.PrimaryKeyRelatedField(
+        queryset=Course.objects.all(), source="course", write_only=True
+    )
 
     class Meta:
         model = Student
-        fields = ["id", "full_name", "email", "password", "roll_no", "course"]
+        fields = [
+            "id",
+            "full_name",
+            "email",
+            "password",
+            "roll_no",
+            "course",
+            "course_id",
+        ]
 
 
 class AuthorSerializers(serializers.ModelSerializer):
