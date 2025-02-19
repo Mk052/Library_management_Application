a2482f199a3521d3f103bfa24ba165fcf8dd5891
madhu <madhukush006@gmail.com>
2025-02-19 15:35:50 +0530

    Added the Book serializer class

--- Bookmanagement/serializers.py
+++ Bookmanagement/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from Bookmanagement.models import Author, Category, Course, Student
+from Bookmanagement.models import Author, Book, Category, Course, Student
 
 
 class StudentSerializers(serializers.ModelSerializer):
@@ -41,3 +41,28 @@ class CourseSerializers(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ["name"]
+
+
+class BookSerializers(serializers.ModelSerializer):
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(), source="author", write_only=True
+    )
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source="category", write_only=True
+    )
+    author = serializers.StringRelatedField(read_only=True)
+    category = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Book
+        fields = [
+            "id",
+            "title",
+            "Description",
+            "author",
+            "category",
+            "is_avaliable",
+            "book_copies",
+            "author_id",
+            "category_id",
+        ]
