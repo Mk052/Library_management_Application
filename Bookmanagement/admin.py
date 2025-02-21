28daf7f8ef4478a3d6a8443ef931bbf6521e6407
madhu <madhukush006@gmail.com>
2025-02-21 19:13:39 +0530

    Register the IssueBook model

--- Bookmanagement/admin.py
+++ Bookmanagement/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Bookmanagement.models import IssueBook, Fine 
 
-# Register your models here.
+
+@admin.register(IssueBook)
+class IssueBookAdmin(admin.ModelAdmin):
+    list_display = ["id", "book", "student", "is_returned", "issue_date", "return_date"]
+
+
+@admin.register(Fine)
+class FineBookAdmin(admin.ModelAdmin):
+    list_display = ["id", "issue_book", "amount"]
